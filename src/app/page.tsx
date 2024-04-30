d048362f11e39a5cb0faf8a214fa4d0f2764bd7b
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-05-01 00:30:29 +0600

    [CHECK] - husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import {Button} from "@nextui-org/button";
+
 import {ThemeSwitcher} from "./theme-switcher";
 
 export default function Home() {
   return (
     <main>
       <ThemeSwitcher />
-      <Button color="primary">CLick me</Button>
+      <Button color="primary">Click me</Button>
     </main>
   );
 }
